655c4e4004078b507528368913a1d14fd021d52d
Bogdan Mart <mart.bogdan@gmail.com>
2015-01-12 23:20:23 +0200

    Added includes to allow build on any system (without masm32 installed)

--- HelloASM/Source.asm
+++ HelloASM/Source.asm
@@ -1,9 +1,9 @@
 .386 
 .model flat,stdcall 
 option casemap:none 
-include \masm32\include\windows.inc 
-include \masm32\include\user32.inc 
-include \masm32\include\kernel32.inc 
+include ..\include\windows.inc 
+include ..\include\user32.inc 
+include ..\include\kernel32.inc 
 ;includelib /masm32/lib/user32.lib
 ;includelib /masm32/lib/kernel32.lib
 WinMain proto :DWORD,:DWORD,:DWORD,:DWORD
